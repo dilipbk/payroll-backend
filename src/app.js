63fa8395d7fb43445ebf75c1495d4c829f01d9eb
Dilip BK <dilipbk.dev@gmail.com>
2024-10-20 18:00:47 +0545

    cors configuration created

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import errorHandler from "./utils/errorHandler.js";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 const app = express();
+
+const corsOptions = {
+  origin: "https://payroll-backend-8faq.onrender.com", // Replace with your frontend domain
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+};
 
 // Middleware
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 
