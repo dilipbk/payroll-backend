51fdc26afcd0b5179ae8ed5e616cfc9508c66696
Dilip BK <dilipbk.dev@gmail.com>
2024-10-20 18:08:02 +0545

    allow origiins array updated

--- src/app.js
+++ src/app.js
@@ -2,15 +2,18 @@ import express from "express";
 import errorHandler from "./utils/errorHandler.js";
 import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
+import { allowedOrigins } from "./config/appConfigs.js";
 const app = express();
 
 const corsOptions = {
-  origin: "https://payroll-backend-8faq.onrender.com", // Replace with your frontend domain
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-  credentials: true,
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
-
 // Middleware
 app.use(express.json());
 app.use(cors(corsOptions));
